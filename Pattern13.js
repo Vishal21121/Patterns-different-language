cf292264d4f5d83b5448e923cff77f8b0772c94d
Vishal21121 <vs84077000@gmail.com>
2022-12-29 19:04:05 +0530

    Added Pattern13.js file

---
+++ Pattern13.js
@@ -0,0 +1,18 @@
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+n = 5
+
+for(i=1;i<=n;i++){
+    for(k=i;k<=n;k++){
+        process.stdout.write(" ")
+    }
+    for(j=1;j<=i;j++){
+        process.stdout.write("*")
+    }
+    console.log()
+    
+}
