1bc1c50604aa17a503502d44165aa3bbd2c651fc
Arsh2001deep <arshdeep.singh.fiem.cse20@teamfuture.in>
2023-01-07 14:39:54 +0530

    added Pattern13.java

--- Pattern13.java
+++ Pattern13.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Pattern13 {
     public static void main(String args[]) {
+
+        System.out.println("enter number:");
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
 
-            for (int j = 2 * (n - i); j >= 0; j--) {
+            for (int k = n-i; k >= 1; k--) {
 
-                System.out.print(" ");
+                System.out.print("3");
             }
 
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
