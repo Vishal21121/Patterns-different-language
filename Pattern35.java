e6319a80454305ad74a0381c29d2b3e82babf69a
Kunal <kunal.bhardwaj@iwedia.com>
2023-05-11 14:54:16 +0530

    Added print statement to ask user for input

--- Pattern35.java
+++ Pattern35.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Pattern35 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
 	        for(int j=1;j<=n-i;j++){
