65dc679e4b6e6236a42e87c7f6f89fef1a201fef
Vishal21121 <vs84077000@gmail.com>
2023-05-14 12:29:21 +0530

    added: Pattern36.js

---
+++ Pattern36.js
@@ -0,0 +1,29 @@
+const readline = require('readline')
+
+n = 7
+for (i = 1; i <= n; i++ ){
+    for (j = 1; j <= n; j++) {
+        if (i <= (n+1)/2) {
+            if (i == 1 && (j == 1 || j >= (n+1)/2)) {
+                process.stdout.write("* ");
+            } else if (i > 1 && j == (n+1)/2 || j == 1) {
+                process.stdout.write("* ");
+            } else if(i==(n+1)/2){
+                process.stdout.write("* ");
+            }
+            else {
+                process.stdout.write("  ");
+            }
+        }else {
+            if(i<n && (j==(n+1)/2 || j==n)){
+                process.stdout.write("* ");
+            }else if (i==n && (j<=(n+1)/2) || j==n){
+                process.stdout.write("* ");
+            }
+            else{
+                process.stdout.write("  ");
+            }
+        }
+    }
+    process.stdout.write("\n");
+}
