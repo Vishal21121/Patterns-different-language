9f09b0838d938b10371a185f32d2263c69488b6c
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2023-05-22 21:51:10 +0530

    Update Pattern39.java

--- Pattern39.java
+++ Pattern39.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Pattern39 {
     static void aPattern(int n) {
-        for (int i = 1; i <= n+2; i++) {
+        for (int i = 1; i <= 2*n-3; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
