299214068f310dadd2bd799f1fd1f7f9e255017b
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2023-05-22 19:11:40 +0530

    Create Pattern39.js

---
+++ Pattern39.js
@@ -0,0 +1,42 @@
+function aPattern(n) {
+  for (let i = 1; i <= n + 2; i++) {
+    for (let j = 1; j <= n - i; j++) {
+      process.stdout.write(" ");
+    }
+
+    if (i === 1) {
+      console.log("*");
+    } else if (i === n) {
+      for (let k = 1; k <= n; k++) {
+        process.stdout.write("* ");
+      }
+      console.log();
+    } else if (i > n) {
+      for (let k = 1; k <= 2 * n - 3; k++) {
+        if (k === 1 || k === 2 * n - 3) {
+          process.stdout.write("* ");
+        } else {
+          process.stdout.write("  ");
+        }
+      }
+      console.log();
+    } else {
+      process.stdout.write("*");
+      for (let k = 1; k <= 2 * i - 3; k++) {
+        process.stdout.write(" ");
+      }
+      console.log("*");
+    }
+  }
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the value of n: ", (n) => {
+  aPattern(parseInt(n));
+  rl.close();
+});
