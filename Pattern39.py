28d5aaac3ef41aa5f1439b386843e23039156310
vishal singh <89929777+Vishal21121@users.noreply.github.com>
2023-05-23 17:52:55 +0530

    Update Pattern39.py

--- Pattern39.py
+++ Pattern39.py
@@ -9,7 +9,7 @@ for i in range(0,rows):
     for j in range(0,columns):
         #the conditions 
         if((j == n-1-i) or (j == n-1+i) or (i>n-1 and j == columns-1 ) or (i>n-1 and j == 0) or (i == n-1 and j%2 == 0 )):
-            print('*',end=' ')
+            print('*',end='')
         else:
-            print(' ',end=' ')
+            print(' ',end='')
     print()
