de720728accd486ae7ea7aff9bc54a63604be988
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2023-05-29 18:50:19 +0300

    Update Pattern42.js
    Now working

--- Pattern42.js
+++ Pattern42.js
@@ -1,4 +1,6 @@
 /*
+  This is the first Aproach
+  
   Before running this code you have to First, install the prompt-sync package 
   by running the following command in your Node.js 
   
@@ -17,33 +19,53 @@ const LETTERS_ALPHABET = 26;
 // Main function
 function main() {
   // Prompt the user to enter a number
-  let n = parseInt(prompt("Please enter a number"));
+  let n = parseInt(prompt("Please enter a number : "));
 
-  // Outer loop for rows
-  for (let i = 0; i < n * 2; i++) {
-    // Check if we are in the first half of rows
-    if (i < n) {
-      // Inner loop for printing letters in ascending order
-      for (let j = 0; j < n - i; j++) {
-        // Calculate the current letter based on the first letter and alphabet size
+for (let i = 0; i < n * 2; i++) {
+      for (let j = 0; j < n - i && i < n; j++) {
         let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
-        console.log(letter + ' ');
+        console.log(`${letter} `);
       }
-    } else { // We are in the second half of rows
-      // Inner loop for printing letters in descending order
-      for (let j = 0; j < i - n + 1; j++) {
-        // Calculate the current letter based on the first letter and alphabet size
+
+      for (let j = 0; j < i - n + 1 && i >= n; j++) {
         let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
-        console.log(letter + ' ');
+        console.log(`${letter} `);
       }
+
+      console.log("\n");
     }
-    // Print a new line after each row
-    console.log('\n');
-  }
 }
 
 // Call the main function to start the program
 main();
 
-// Run the main function when the page is loaded
-// window.addEventListener('load', main);
+/* Second Aproach
+
+const readline = require('readline');
+
+const FIRST_LETTER = 'A';
+const LETTERS_ALPHABET = 26;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+input.question("Please enter a number\n", (n) => {
+  for (let i = 0; i < n * 2; i++) {
+    for (let j = 0; j < n - i && i < n; j++) {
+      let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
+      process.stdout.write(`${letter} `);
+    }
+
+    for (let j = 0; j < i - n + 1 && i >= n; j++) {
+      let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
+      process.stdout.write(`${letter} `);
+    }
+
+    process.stdout.write("\n");
+  }
+
+  rl.close();
+});
+*/
