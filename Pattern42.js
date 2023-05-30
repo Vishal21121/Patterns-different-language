83d84d47db0cf4100dfa02425d828f685b54e449
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2023-05-30 16:57:53 +0300

    Update Pattern42.js
    removed the first approach

--- Pattern42.js
+++ Pattern42.js
@@ -1,45 +1,3 @@
-/*
-  This is the first Aproach
-  
-  Before running this code you have to First, install the prompt-sync package 
-  by running the following command in your Node.js 
-  
-  *** npm install prompt-sync ***
-
-*/
-
-
-const prompt = require('prompt-sync')();
-
-// Constants for the first letter and number of letters in the alphabet
-
-const FIRST_LETTER = 'A';
-const LETTERS_ALPHABET = 26;
-
-// Main function
-function main() {
-  // Prompt the user to enter a number
-  let n = parseInt(prompt("Please enter a number : "));
-
-for (let i = 0; i < n * 2; i++) {
-      for (let j = 0; j < n - i && i < n; j++) {
-        let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
-        console.log(`${letter} `);
-      }
-
-      for (let j = 0; j < i - n + 1 && i >= n; j++) {
-        let letter = String.fromCharCode(FIRST_LETTER.charCodeAt(0) + (j % LETTERS_ALPHABET));
-        console.log(`${letter} `);
-      }
-
-      console.log(" ");
-    }
-}
-
-// Call the main function to start the program
-main();
-
-/* Second Aproach
 
 const readline = require('readline');
 
@@ -68,4 +26,4 @@ input.question("Please enter a number\n", (n) => {
 
   input.close();
 });
-*/
+
