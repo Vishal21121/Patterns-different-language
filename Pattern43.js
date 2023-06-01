ba89f075a4107bee5687d3fbdbca8812a3cd3086
Okellosam21 <samuel.okello@studentambassadors.com>
2023-06-01 11:23:29 +0300

    add: pascal function

--- Pattern43.js
+++ Pattern43.js
@@ -1,1 +1,18 @@
-var generate = function(){}
+var generate = function(numRows){
+    var result = [];
+    if(numRows == 0)
+     return result;
+     result.push([1]);
+     for(var i = 1; i < numRows; i++){
+        let list = [];
+        list.push(1);
+        for(var j = 1; j < result[i-1].length; j++){
+            let sum = result[i-1][j-1] + result[i-1][j];
+            list.push(sum);
+        }
+        list.push(1);
+        result.push([...list]);
+    }
+    return result;
+
+}
