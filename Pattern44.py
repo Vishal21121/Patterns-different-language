6a7d3134649cbfc5f1ed922d64278f7eebe1d471
HeatherWatson1 <heatherwatson828@yahoo.com>
2023-06-03 11:59:08 -0500

    Update Pattern44.py
    Removed extra line from star pattern.

--- Pattern44.py
+++ Pattern44.py
@@ -4,7 +4,6 @@
 # *   *
 # *   *
 #  ***
-#  ***
 # *   *
 # *   *
 # *   *
@@ -14,7 +13,7 @@
 n = int(input("Enter the number:"))
 
 for i in range(1, (n*2)+1 ):
-    if i == 1 or i == n or i == n+1 or i == n*2: #First, last and middle lines
+    if i == 1 or i == n or i == n*2: #First, last and middle lines
         print(" " + "*"*(n-2))
     else:
         print("*"+" "*(n-2)+"*")
