43644cab119ffb71e40a6ce1692f57fa4e32efab
HeatherWatson1 <heatherwatson828@yahoo.com>
2023-06-04 18:20:33 -0500

    Update Pattern44.py

--- Pattern44.py
+++ Pattern44.py
@@ -12,8 +12,8 @@
 
 n = int(input("Enter the number:"))
 
-for i in range(1, (n*2)+1 ):
-    if i == 1 or i == n or i == n*2: #First, last and middle lines
+for i in range(1, (n*2) ):
+    if i == 1 or i == n or i == n*2 - 1: #First, last and middle lines
         print(" " + "*"*(n-2))
     else:
         print("*"+" "*(n-2)+"*")
