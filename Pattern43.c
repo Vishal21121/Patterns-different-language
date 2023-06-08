95db5237f91f988bd79b6f2458c641fb0a43f440
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2023-06-08 14:41:53 +0530

    Update Pattern43.c

--- Pattern43.c
+++ Pattern43.c
@@ -2,10 +2,10 @@
 
 int factorial(int n) {
    int f;
-   
+
    for(f = 1; n > 1; n--)
       f *= n;
-      
+
    return f;
 }
 
@@ -19,10 +19,10 @@ int main() {
   scanf("%d",&n);
 
 
-   for(i = 0; i <= n; i++) {
-      for(j = 0; j <= n-i; j++)
+   for(i = 0; i < n; i++) {
+      for(j = 0; j < n-i; j++)
          printf("  ");
-         
+
       for(j = 0; j <= i; j++)
          printf(" %3d", ncr(i, j));
 
