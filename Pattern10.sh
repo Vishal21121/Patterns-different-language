fa4d5dd3b5e924a9f0e213be9294a8270aa49974
aneesh27 <98216256+aneeshd27@users.noreply.github.com>
2023-06-09 23:04:37 +0530

    Create Pattern10.sh

---
+++ Pattern10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read num
+for((i=0;i<num;i++))
+do
+    for((j=0;j<=i;j++))
+    do
+        char_value=$((j + 65))
+        char=$(printf "\\$(printf '%03o' $char_value)")
+        printf "%s " "$char"
+    done
+    echo " "
+done    
