0be13662c440881cff45af32021dd423e331776c
Raghav Limbani <itsrgcodes@gmail.com>
2023-06-14 23:12:35 +0530

    Patterns1.py added

---
+++ Pattern1.py
@@ -0,0 +1,535 @@
+[33mcommit 3fdbd67aa3a2ac16beaa877045e27989a4fa92db[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Raghav Limbani <itsrgcodes@gmail.com>
+Date:   Mon Jun 12 21:37:46 2023 +0530
+
+    added Python file for Pattern1
+
+[33mcommit 791b891cad3d3d2432e7ac626f350f3292b72f93[m[33m ([m[1;31morigin/Pattern1-pyhton[m[33m)[m
+Merge: 7e174c6 4a8d2f2
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Mon Jun 12 13:06:38 2023 +0530
+
+    Merge pull request #144 from Alkaison/Feat-created-Pattern1-in-C-language
+    
+    Feat: created Pattern1 in C language
+
+[33mcommit 4a8d2f2b439f3564eb002046705b957883603d06[m
+Author: Ganesh Mourya <505ganeshmourya@gmail.com>
+Date:   Sun Jun 11 21:32:26 2023 +0530
+
+    Feat: created Pattern1 in C language
+
+[33mcommit 7e174c67842e08e9e0df38785a8f4529e960bb5d[m
+Merge: 6d33d73 19dd4ab
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sat Jun 10 18:30:34 2023 +0530
+
+    Merge pull request #141 from aneeshd27/master
+    
+    added: Pattern24, Pattern29.sh
+
+[33mcommit 19dd4abf5848daa5f4fdc35f7418346c3c8e9fcf[m
+Author: anee27 <aneeshdighe27@gmail.com>
+Date:   Sat Jun 10 18:06:15 2023 +0530
+
+    Add Pattern24.sh and Pattern29.sh
+
+[33mcommit 6d33d73c974ffb865da962806f02904fdb99f05e[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 17:23:09 2023 +0530
+
+    modified readme
+
+[33mcommit 02c208ef02fa36cb15e1fa16618f28b3cee22771[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 17:20:38 2023 +0530
+
+    added: Pattern44.png, shifted Pattern1 files
+
+[33mcommit 02f6c1bfc16f61adc9584f748c83101972faeeb7[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 17:01:35 2023 +0530
+
+    shifted Patten26 to 44 from root
+
+[33mcommit 4782bdad9e7372d4f6978e2a7b6d9641eb599f5d[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 16:49:58 2023 +0530
+
+    removed Pattern files till 25 from root
+
+[33mcommit 541d043eef1a384e45e0818859dff157fc0b3b59[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 16:45:01 2023 +0530
+
+    removed c files
+
+[33mcommit 7b88c0f5b398d1405dbb3e193440c06a1c066de1[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 16:31:45 2023 +0530
+
+    shifted: Pattern5 to Pattern25
+
+[33mcommit 0caae71b923fd66fa02a90036e039ed51a4e3d4e[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 10 16:11:24 2023 +0530
+
+    shifted: Pattern2, Pattern3, Pattern4
+
+[33mcommit 7c69e03d8cd66c7e624d5316b2a786b0ee870253[m
+Merge: d77de01 e7ed8e1
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sat Jun 10 15:25:50 2023 +0530
+
+    Merge pull request #128 from OkelloSam21/master
+    
+    added: Pattern43.js
+
+[33mcommit e7ed8e1f2cf6a0b7657e32641d53922bbbc576b6[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sat Jun 10 11:38:05 2023 +0300
+
+    Update Pattern43.js
+    
+    removed the `trim()` function from line 32
+
+[33mcommit d77de0169f25213c12ec696819e244aa3691941c[m
+Merge: e7765ae 133cc63
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sat Jun 10 12:18:21 2023 +0530
+
+    Merge pull request #129 from aneeshd27/master
+    
+    added: Pattern10.sh
+
+[33mcommit 133cc633a19ab8193ae8ee4581380f50b5c82d7f[m
+Author: aneesh27 <98216256+aneeshd27@users.noreply.github.com>
+Date:   Fri Jun 9 23:03:01 2023 +0530
+
+    Update Pattern10.sh
+
+[33mcommit e7765ae55afb8fc39e736b73c29fb4689d1fd860[m
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri Jun 9 20:27:25 2023 +0530
+
+    Rename Patten.js to Pattern1.js
+
+[33mcommit d17aa5acd2199570225a31390e9d43c042bee385[m
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri Jun 9 20:26:09 2023 +0530
+
+    Rename Pattern.go to Pattern1.go
+
+[33mcommit 869cd43567423686747ede593f98328f44289a65[m
+Merge: 3ce3036 95db523
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri Jun 9 19:04:51 2023 +0530
+
+    Merge pull request #124 from abhishek09827/master-8
+    
+    added: Pattern43.c
+
+[33mcommit 3ce3036369bc1c19dda3057c38c6f4e9376c7efe[m
+Merge: b422666 0e065ed
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri Jun 9 19:01:03 2023 +0530
+
+    Merge pull request #122 from abhishek09827/master-6
+    
+    added: Pattern43.java
+
+[33mcommit 95db5237f91f988bd79b6f2458c641fb0a43f440[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Thu Jun 8 14:41:53 2023 +0530
+
+    Update Pattern43.c
+
+[33mcommit 0e065ed8de7605f2c3d21d6e0156300f06d48673[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Thu Jun 8 14:37:54 2023 +0530
+
+    Update Pattern43.java
+
+[33mcommit 493cf8ad7c14f95779cb4ae8273c00e972fbb462[m
+Author: aneesh27 <98216256+aneeshd27@users.noreply.github.com>
+Date:   Thu Jun 8 12:28:45 2023 +0530
+
+    Add files via upload
+
+[33mcommit b4226669c15ae2d2c4d18cc284dfffa5e214cd38[m
+Merge: d2aae47 7fe7012
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Thu Jun 8 07:46:29 2023 +0530
+
+    Merge pull request #123 from abhishek09827/master-7
+    
+    added: Pattern43.py
+
+[33mcommit d2aae47a9204921a5bed5337f10d5eff51e65bc9[m
+Merge: 88663b3 555713c
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Thu Jun 8 07:43:49 2023 +0530
+
+    Merge pull request #125 from Harshal662/pattern44
+    
+    added: Pattern44.c
+
+[33mcommit 555713cd717733b7da49ed670767ddf7ce319b55[m
+Author: Harshal Abak <abakharshal@gmail.com>
+Date:   Wed Jun 7 22:23:24 2023 +0530
+
+    Rename pattern44.c to Pattern44.c
+
+[33mcommit a6ee29f2f39c1a05df59248cfd1ae8b0a4794779[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Wed Jun 7 16:49:54 2023 +0000
+
+    update Pattern43.js
+
+[33mcommit a6c69234657e61ca15c348a468b6810d6d1ed6a2[m
+Merge: ba89f07 88663b3
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Wed Jun 7 18:59:21 2023 +0300
+
+    Merge branch 'Vishal21121:master' into master
+
+[33mcommit 88663b3b2937bfa54a16be84fb634de633940be0[m
+Merge: 07e9727 43644ca
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Mon Jun 5 22:49:52 2023 +0530
+
+    Merge pull request #120 from HeatherWatson1/master
+    
+    added: Pattern44.py
+
+[33mcommit 43644cab119ffb71e40a6ce1692f57fa4e32efab[m
+Author: HeatherWatson1 <heatherwatson828@yahoo.com>
+Date:   Sun Jun 4 18:20:33 2023 -0500
+
+    Update Pattern44.py
+
+[33mcommit 07e9727d8635903f9a174d32afaa823da2daf06b[m
+Merge: 54cead6 6da8e01
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sun Jun 4 08:53:16 2023 +0530
+
+    Merge pull request #121 from mtu4172/master
+    
+    added: Pattern44.java
+
+[33mcommit aef52e8df467a702089151c7f580c6611103fc42[m
+Author: Harshal Abak <abakharshal@gmail.com>
+Date:   Sat Jun 3 23:51:15 2023 +0530
+
+    pattern44.c
+
+[33mcommit 6a7d3134649cbfc5f1ed922d64278f7eebe1d471[m
+Author: HeatherWatson1 <heatherwatson828@yahoo.com>
+Date:   Sat Jun 3 11:59:08 2023 -0500
+
+    Update Pattern44.py
+    
+    Removed extra line from star pattern.
+
+[33mcommit 6da8e01a015d3185a2ca38c98135cba83e19e926[m
+Author: Michael Tu <michaeltu12345@gmail.com>
+Date:   Sat Jun 3 08:06:59 2023 -0500
+
+    added print statement
+
+[33mcommit 3a9ff286f923fb596c5de6e5b31f84d963e68f54[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Sat Jun 3 16:31:15 2023 +0530
+
+    Create Pattern43.c
+
+[33mcommit d2f6b310dbf944ea159da24f53d5c989e1d173fa[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Sat Jun 3 16:27:33 2023 +0530
+
+    Update Pattern43.java
+
+[33mcommit 7fe7012772337b20a0e4fb285fb63cbdb2a06bf9[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Sat Jun 3 16:26:38 2023 +0530
+
+    Create Pattern43.py
+
+[33mcommit 7fd2e1b3c2a634c247abc32aa19dca9815c9e641[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Sat Jun 3 16:23:12 2023 +0530
+
+    Create Pattern43.java
+
+[33mcommit d27790eb02021f159b5d05a99dabd8196aed3fba[m
+Author: Michael Tu <michaeltu12345@gmail.com>
+Date:   Sat Jun 3 02:16:07 2023 -0500
+
+    added Pattern44.java file
+
+[33mcommit 5b43dda8d5b893a158332eb9ba6479e35f5edb75[m
+Author: HeatherWatson1 <heatherwatson828@yahoo.com>
+Date:   Fri Jun 2 23:52:42 2023 -0500
+
+    Create Pattern44.py
+    
+    Code that prints an Eight shape star pattern in Python
+
+[33mcommit 54cead6749c42172b0c0358f062584bd3500bd9e[m
+Merge: a62b30a 3eab118
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sat Jun 3 08:50:56 2023 +0530
+
+    Merge pull request #119 from akansjain/master
+    
+    added: Pattern44.js
+
+[33mcommit 3eab118ac39f2b7c8ca0e007a98141d59264e6ed[m
+Author: akansjain <100862574+akansjain@users.noreply.github.com>
+Date:   Sat Jun 3 08:13:10 2023 +0530
+
+    Created Pattern44.js
+
+[33mcommit a62b30a78e95ad1a9844032ede361c1549f605c8[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat Jun 3 07:36:29 2023 +0530
+
+    added: Pattern44.go
+
+[33mcommit ba89f075a4107bee5687d3fbdbca8812a3cd3086[m
+Author: Okellosam21 <samuel.okello@studentambassadors.com>
+Date:   Thu Jun 1 11:23:29 2023 +0300
+
+    add: pascal function
+
+[33mcommit 661148a7ab1a77bb234c9b9a5dec97ce24bd69bb[m
+Author: Okellosam21 <samuel.okello@studentambassadors.com>
+Date:   Thu Jun 1 11:11:47 2023 +0300
+
+    add: Pattern43.js
+
+[33mcommit 951eb4d4fe1a2e89467fba8619fcac847dc86a21[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Wed May 31 13:24:09 2023 +0530
+
+    added: 43.png
+
+[33mcommit c6362d35eaf96065bef62aed1152dc54fd70fadb[m
+Merge: ea7f4ad 57584a3
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Tue May 30 19:53:53 2023 +0530
+
+    Merge pull request #114 from abhishek09827/master-5
+    
+    added Pattern42.java
+
+[33mcommit ea7f4ad58b9d12cd37f7d4462b7ddaf2521e00e6[m
+Merge: 78cd7ee 83d84d4
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Tue May 30 19:46:32 2023 +0530
+
+    Merge pull request #109 from OkelloSam21/master
+    
+    added Pattern42.py and Pattern42.js
+
+[33mcommit 83d84d47db0cf4100dfa02425d828f685b54e449[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Tue May 30 16:57:53 2023 +0300
+
+    Update Pattern42.js
+    
+    removed the first approach
+
+[33mcommit 57584a3d483985934a38b31a6ab7b2a5144f75bc[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Tue May 30 17:50:08 2023 +0530
+
+    Create Pattern42
+
+[33mcommit 283662bf8833eb66ce3842a473e929bca18f67e5[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Mon May 29 19:32:53 2023 +0300
+
+    Update Pattern42.js
+
+[33mcommit de720728accd486ae7ea7aff9bc54a63604be988[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Mon May 29 18:50:19 2023 +0300
+
+    Update Pattern42.js
+    
+    Now working
+
+[33mcommit 127879971db199b0d540e9684dd4e1cf3785cab2[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Mon May 29 17:55:15 2023 +0300
+
+    Update Pattern42.js
+    
+    added comment at the beginning of the code for the user to install `prompt-sync package` to allow usage of prompt.
+
+[33mcommit 78cd7ee5548582766a0df8bfeed478587f1c73c9[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Mon May 29 19:36:52 2023 +0530
+
+    added: 41,42 png files
+
+[33mcommit b2e9dfeab28c7409fd880ba2c6926b0b1e3e6fc1[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Mon May 29 09:10:17 2023 +0530
+
+    added: Pattern42.go
+
+[33mcommit 2bc9dd03aa2b3146e4366333b9a8cac9544e5bb2[m
+Merge: 849902e 32ab952
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 19:57:12 2023 +0300
+
+    Merge pull request #4 from OkelloSam21/OkelloSam21-patch-4
+    
+    Update Pattern42.js
+
+[33mcommit 32ab95205c9bf4afe2d8d37c7dbe90e481b63233[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 19:54:33 2023 +0300
+
+    Update Pattern42.js
+
+[33mcommit 849902eef18a147fccfd433f4e690f716c997535[m
+Merge: 1681267 090a088
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 17:24:02 2023 +0300
+
+    Merge pull request #3 from OkelloSam21/OkelloSam21-patch-3
+    
+    Create Pattern42.js
+
+[33mcommit 090a088e08b324d974f5d04491d56e6323b2fb45[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 17:20:17 2023 +0300
+
+    Create Pattern42.js
+
+[33mcommit 1681267e04df9d5fd7a8f68613ee819dd7f0af26[m
+Merge: 7c175bc 33dee1e
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 17:16:54 2023 +0300
+
+    Merge pull request #2 from OkelloSam21/OkelloSam21-patch-2
+    
+    Rename pattern42.py to Pattern42.py
+
+[33mcommit 33dee1ee9cf7875a002e82ad70f799bfff380f11[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 17:15:00 2023 +0300
+
+    Rename pattern42.py to Pattern42.py
+
+[33mcommit 7c175bc38a827fcbd608506a7f466e240240b07a[m
+Merge: 21f97be f41482c
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 16:20:36 2023 +0300
+
+    Merge pull request #1 from OkelloSam21/OkelloSam21-patch-1
+    
+    Create pattern42.py
+
+[33mcommit f41482cb0fbc509d9cfe31c73700a7783d2b74e6[m
+Author: SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
+Date:   Sun May 28 06:09:46 2023 +0300
+
+    Create pattern42.py
+
+[33mcommit 21f97bef30a2e68a5fb395f5f260d2738bdc9f15[m
+Merge: b465fe1 c27b323
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Sun May 28 07:16:54 2023 +0530
+
+    Merge pull request #106 from GiuliaBortone/add-pattern
+    
+    Added Pattern42.c file
+
+[33mcommit c27b32316a772fd910d002e7f530869719d137f3[m
+Author: GiuliaBortone <giulia.bortone@mail.polimi.it>
+Date:   Sat May 27 21:46:09 2023 +0200
+
+    Added Pattern42 in C file
+
+[33mcommit b465fe18fad81679f496ed2bf58da16f937bcd45[m
+Author: Vishal21121 <vs84077000@gmail.com>
+Date:   Sat May 27 19:49:34 2023 +0530
+
+    added: Pattern41.go
+
+[33mcommit c60f5408656f3f4a1bfaa4a3188fe5751b6d1a82[m
+Merge: 77778a6 1868843
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri May 26 10:34:10 2023 +0530
+
+    Merge pull request #103 from achhayapathak/master
+    
+    added Pattern41.c,js,py,java file
+
+[33mcommit 1868843f35a8b321bc3e45b51ac380be20bd6482[m
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Fri May 26 10:32:45 2023 +0530
+
+    deleted task.json
+
+[33mcommit 0da1856a9ce3aa532f39c994ffed2f4ed5ef3ad5[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Fri May 26 09:46:17 2023 +0530
+
+    Deleted unnecessary files
+
+[33mcommit 46494eb9b0a0e87c11b8b93c82cb447735334049[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Thu May 25 22:46:33 2023 +0530
+
+    fixed errors
+
+[33mcommit 0dcbfabf66ef4799f3b76719558f664e6417ed6a[m
+Merge: b0a2b36 77778a6
+Author: Achhaya Pathak <93811742+achhayapathak@users.noreply.github.com>
+Date:   Thu May 25 20:29:50 2023 +0530
+
+    Merge branch 'Vishal21121:master' into master
+
+[33mcommit b0a2b36bc8f729056ddc74f63c57d678352aadf9[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Thu May 25 20:26:09 2023 +0530
+
+    Added Patter41.py
+
+[33mcommit dc7794af26f04e029724d0ef2d46be9f29da5f90[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Thu May 25 20:11:32 2023 +0530
+
+    Added pattern41.c
+
+[33mcommit 77778a6296508611c3095281b4bfd5eea78c2c6b[m
+Merge: 4dd9faa fd97dbf
+Author: vishal singh <89929777+Vishal21121@users.noreply.github.com>
+Date:   Thu May 25 19:28:19 2023 +0530
+
+    Merge pull request #99 from abhishek09827/master-4
+    
+    added Pattern40.js
+
+[33mcommit fd97dbfd0ce6d9e680cae109aaa8bb24b0fc803b[m
+Author: abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
+Date:   Thu May 25 18:53:44 2023 +0530
+
+    Create Pattern40.js
+
+[33mcommit 6cd6cfd10d669bac7db11e0819cbf066d7a40304[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Thu May 25 17:19:48 2023 +0530
+
+    Added Pattern41.js
+
+[33mcommit 68abae80bcefcc990043f18a95f015dac949c4f7[m
+Author: Achhaya Pathak <achhayapathak11@gmail.com>
+Date:   Thu May 25 09:13:57 2023 +0530
+
+    Added Pattern41.java
