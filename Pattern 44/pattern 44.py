c01a7d2fa29700c7788eb72e667ce317b0899f19
Gauri Tripathi <129727667+GauriTr@users.noreply.github.com>
2023-06-15 10:12:34 +0530

    Delete Pattern 44 directory

--- Pattern 44/pattern 44.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Enter the number of rows: "))
-
-for i in range(rows):
-    print(" " * (rows - i - 1) + "* " * (i + 1))
-
-for i in range(rows - 2, -1, -1):
-    print(" " * (rows - i - 1) + "* " * (i + 1))
