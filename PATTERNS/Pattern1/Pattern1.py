92482d3cf1aef2f367c6cbe322441ea497da935d
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-15 15:03:56 -0400

    added first pattern for python

---
+++ PATTERNS/Pattern1/Pattern1.py
@@ -0,0 +1,8 @@
+def print_hourglass(size):
+    for i in range(size, 0, -1):
+        print(" " * (size - i) + "*" * (2 * i - 1))
+    for i in range(2, size + 1):
+        print(" " * (size - i) + "*" * (2 * i - 1))
+
+size = input("input the size: ")
+print_hourglass(size)
