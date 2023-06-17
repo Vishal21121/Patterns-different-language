cdb5925904e0bf256c6d3198a4c1e4d7017c4411
Gauri Tripathi <129727667+GauriTr@users.noreply.github.com>
2023-06-17 21:28:53 +0530

    Update Pattern45.java

--- PATTERNS/Pattern45/Pattern45.java
+++ PATTERNS/Pattern45/Pattern45.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+public class Main{
+public static void main(String[] args) {
+Scanner z= new Scanner(System.in);
+System.out.println("Enter the Number of rows: ");
+int rows = z.nextInt(); // Number of rows in the rhombus
+    for (int i = 1; i <= rows; i++) {
+        // Print spaces
+        for (int j = 1; j <= rows - i; j++) {
+            System.out.print(" ");
         }
 
-        for (int i = rows - 2; i >= 0; i--) {
-            for (int j = 0; j < rows - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
+        // Print stars for left diagonal
+        for (int k = 1; k <= i; k++) {
+            System.out.print("*");
         }
+
+        // Print stars for right diagonal
+        for (int k = i - 1; k >= 1; k--) {
+            System.out.print("*");
+        }
+
+        System.out.println(); // Move to the next line
     }
+
+    for (int i = rows - 1; i >= 1; i--) {
+        // Print spaces
+        for (int j = 1; j <= rows - i; j++) {
+            System.out.print(" ");
+        }
+
+        // Print stars for left diagonal
+        for (int k = 1; k <= i; k++) {
+            System.out.print("*");
+        }
+
+        // Print stars for right diagonal
+        for (int k = i - 1; k >= 1; k--) {
+            System.out.print("*");
+        }
+
+        System.out.println(); // Move to the next line
+    }
+}
 }
