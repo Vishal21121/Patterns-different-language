f30cf172d5253a0c4841d5459a1c751bbdf92925
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-19 13:44:54 -0400

    added pattern 45 in c

---
+++ PATTERNS/Pattern45/Pattern45.c
@@ -0,0 +1,37 @@
+//adding 45th diamond pattern in c
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <stdio.h>
+int main()
+{
+  int n, c, k;
+
+  printf("Enter number of rows\n");
+  scanf("%d", &n);
+
+  for (k = 1; k <= n; k++)
+  {
+    for (c = 1; c <= n-k; c++)
+      printf(" ");
+
+    for (c = 1; c <= 2*k-1; c++)
+      printf("*");
+
+    printf("\n");
+  }
+
+  for (k = 1; k <= n - 1; k++)
+  {
+    for (c = 1; c <= k; c++)
+      printf(" ");
+
+    for (c = 1 ; c <= 2*(n-k)-1; c++)
+      printf("*");
+
+    printf("\n");
+  }
+
+  return 0;
+}
