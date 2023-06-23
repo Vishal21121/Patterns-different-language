01987b145bf484cffb518ec3454af5ce5b57dc1f
Samuel <samuel.okello@gmail.com>
2023-06-23 12:30:19 +0300

    add logic

--- Pattern48.java
+++ Pattern48.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 public class Pattern48{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows : ");
+        int rows = sc.nextInt();
+
+        int count = 1;
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
     }
 }
