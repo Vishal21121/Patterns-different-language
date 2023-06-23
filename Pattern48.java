8a862afbd8cfb003f33be9d254d5d9c933027846
Samuel <samuel.okello@gmail.com>
2023-06-23 13:57:11 +0300

    update pattern48.java

--- Pattern48.java
+++ Pattern48.java
@@ -1,18 +1,46 @@
 import java.util.Scanner;
-public class Pattern48{
+
+public class Pattern48 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter the number of rows : ");
         int rows = sc.nextInt();
+        printPattern(rows);
 
-        int count = 1;
-        for(int i = 1; i <= rows; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print(count + " ");
-                count++;
-            }
-            System.out.println();
-        }
+    }
+
+    static void printPattern(int n) 
+    { 
+        int col, num = 0; 
+ 
+        // loop for row
+        for (int row = 1; row <= n; row++) { 
+ 
+            // when row number is odd,then print in increasing order.
+            if (row % 2 != 0) { 
+ 
+                // printing in ascending order
+                for (col = num + 1; col < num + row; col++) 
+                    System.out.print(col + " "); 
+                System.out.println(col++); 
+ 
+                // update value of 'num' 
+                num = col; 
+            } 
+ 
+         // when row number is odd,then print in descending order.
+            else { 
+ 
+                // update value of 'num' 
+                num = num + row - 1; 
+ 
+                // print numbers with the ' ' in 
+                // decreasing order 
+                for (col = num; col > num - row + 1; col--) 
+                    System.out.print(col + " "); 
+                System.out.println(col); 
+            } 
+        } 
     }
 }
