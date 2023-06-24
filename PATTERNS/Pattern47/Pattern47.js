ae5c184b84c46c4ebd5f37c34f68150067ca5447
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-23 20:48:52 -0400

    fixed pattern 47 and added user input

--- PATTERNS/Pattern47/Pattern47.js
+++ PATTERNS/Pattern47/Pattern47.js
@@ -1,13 +1,11 @@
-function generatePattern() {
+function generatePattern(input) {
     let pattern = '';
     // let input = prompt("enter a number: ");
 
     //adding an example number since the prompt function isn't working
-
-    let input = 7;
   
-    for (let i = input; i >= 1; i--) {
-      for (let j = input; j >= 1; j--) {
+    for (let i = 1; i<=input; i++) {
+      for (let j = input; j>=1; j--) {
         if (j === i) {
           pattern += '*';
         } else {
@@ -19,4 +17,21 @@ function generatePattern() {
   
     return pattern;
   }
-  console.log(generatePattern());
+
+
+//readline is an object, we use the built in require function in js to fetch the readline module
+
+var readline = require('readline'); 
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('pick a number ', (n) => {
+  
+    var pattern = generatePattern(parseInt(n));
+    console.log(pattern);
+    rl.close();
+  
+});
+
