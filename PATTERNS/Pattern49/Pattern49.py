dff1ce4cc4dcf640c03b1302e154c8da061c0f49
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-06-26 15:49:23 +0000

    Added pattern 49

---
+++ PATTERNS/Pattern49/Pattern49.py
@@ -0,0 +1,26 @@
+
+def isPrime(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+def prime(count):
+    i = 0
+    while(True):
+        i += 1
+        if isPrime(i):
+            if count == 1:
+                return i
+            count -= 1
+
+n = int(input("Enter the number of rows:"))
+
+count = 1
+        
+for i in range(n):
+    print(" " * (n-i), end="")
+    for j in range(i):
+        print(prime(count),end = " ")
+        count += 1
+    print()
