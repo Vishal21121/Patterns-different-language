1feace452f1b510ab09a09f99ecd6bfa1c7843a8
Raafeh <raafehsajjad13@gmail.com>
2023-06-28 00:06:22 +0500

    changes to row rectification

--- PATTERNS/Pattern50/Pattern50.py
+++ PATTERNS/Pattern50/Pattern50.py
@@ -2,8 +2,8 @@ n = int(input("Enter the number of rows:"))
 
 def print_pattern(n):
     num = 2
-    for i in range(9):
-        if i % 4 == 0:
+    for i in range(2*n-1):
+        if i==0 or i==n-1 or i==2*n-2:
             for j in range(n):
                 print(num, end=' ')
                 num += 2
