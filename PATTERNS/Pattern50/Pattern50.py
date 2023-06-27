b74965ef4d31d9e6918bb87acbb92d98a31d0168
Raafeh <raafehsajjad13@gmail.com>
2023-06-28 00:19:49 +0500

    row logic rectified

--- PATTERNS/Pattern50/Pattern50.py
+++ PATTERNS/Pattern50/Pattern50.py
@@ -2,13 +2,12 @@ n = int(input("Enter the number of rows:"))
 
 def print_pattern(n):
     num = 2
-    for i in range(2*n-1):
-        if i==0 or i==n-1 or i==2*n-2:
-            for j in range(n):
+    for i in range(2 * n - 1):
+        if i % (n - 1) == 0:
+            for _ in range(n):
                 print(num, end=' ')
                 num += 2
             print()
-            
         else:
             print(num)
             num += 2
