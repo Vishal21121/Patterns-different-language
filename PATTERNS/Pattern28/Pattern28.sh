c4c0073a82dc3f1227fe39ba30df769c914bf337
Vidhi Mantri <92944931+vmantri83@users.noreply.github.com>
2024-03-30 11:33:54 +0530

    Create Pattern28.sh

---
+++ PATTERNS/Pattern28/Pattern28.sh
@@ -0,0 +1,12 @@
+echo -n "Enter the number of rows:"
+read n
+local alphabet=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+    
+    for ((i=n; i>=1; i--)); 
+    do
+        for ((j=i; j<=n; j++)); 
+	do
+            echo -n "${alphabet[j-1]} "
+        done
+        echo
+    done
